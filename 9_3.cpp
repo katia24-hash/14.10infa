d7079ef1fa574566b679848566ed0f2d32f7a37f
kateee <katia24chili@gmail.com>
2025-12-02 12:13:19 +0300

    6

--- 9_3.cpp
+++ 9_3.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 
-long long power(long long a, int n) {
-    if (n == 0) 
-        return 1;
-    return a * power(a, n - 1);
+double power(double a, int n) {
+    if (n == 0)
+        return 1.0;
+    if (n > 0)
+        return a * power(a, n - 1);
+    else
+        return 1.0 / power(a, -n);
 }
 
 int main() {
-    long long a;
+    double a;
     int n;
     
     std::cout << "Введите основание a: ";
     std::cin >> a;
     
-    std::cout << "Введите показатель степени n (неотрицательное целое): ";
+    std::cout << "Введите показатель степени n: ";
     std::cin >> n;
     
     std::cout << "Результат a^n: " << power(a, n) << std::endl;
